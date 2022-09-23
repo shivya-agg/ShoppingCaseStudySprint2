7b292aa22c62daa1894cb940bc6a08ff12d4e7bc
shivya-agg <shivya.aggarwal@capgemini.com>
2022-09-23 10:28:50 -0700

    Sprint2CategoryUpdates

--- CaseStudySprint2/CategoryTableViewController.swift
+++ CaseStudySprint2/CategoryTableViewController.swift
@@ -6,15 +6,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class CategoryTableViewController: UITableViewController {
 
-   
-    
+   //creating category data array
+    var categroryData = [String]()
+    var categoryImageArray = ["mobile.png","authentication.png", "loginUser.png", "signupMail.png", "signupUser.png", "signupUserPassword.png","mobile.png","authentication.png", "loginUser.png", "signupMail.png", "signupUser.png", "signupUserPassword.png","mobile.png","authentication.png", "loginUser.png", "signupMail.png", "signupUser.png", "signupUserPassword.png","signupUser.png", "signupUserPassword.png"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        alamofireJsonData()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -25,22 +27,53 @@ class CategoryTableViewController: UITableViewController {
         self.tableView.register(UINib(nibName: "CategoryTableViewCell", bundle: nil), forCellReuseIdentifier: "CategoryTableViewCell")
     }
 
+    
+    //Calling Category API
+    func alamofireJsonData() {
+        
+        Alamofire.request("https://dummyjson.com/products/categories", method: .get, encoding: URLEncoding.default, headers: nil).responseJSON { response in
+            switch response.result {
+                case .success:
+                if let categoryDict: NSArray = response.value as! NSArray? {
+                    print(categoryDict)
+                    self.categroryData = categoryDict as! [String]
+                    
+                    if self.categroryData.count > 0 {
+                        self.tableView.reloadData()
+                    }
+                }
+                break
+                case .failure(let error):
+                    print(error)
+            }
+        }
+        
+    }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
+        
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return categroryData.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! CategoryTableViewCell
-
+        
+        //setting label to category
+        let dict = categroryData[indexPath.row]
+        cell.categoryLabel.text = dict as String
+        
+        //adding category image
+        let categoryImage = categoryImageArray[indexPath.row]
+        cell.imageView?.image = UIImage(named: categoryImage)
         // Configure the cell...
 
         return cell
@@ -51,7 +84,11 @@ class CategoryTableViewController: UITableViewController {
         let addToCartViewController = self.storyboard?.instantiateViewController(withIdentifier: "addToCartViewController") as! AddToCartViewController
         self.navigationController?.pushViewController(addToCartViewController, animated: true)
     }
-
+    
+ /*   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+*/
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {

02245480c5761e7052ac73db02cbf9eb93621ced
shivya-agg <shivya.aggarwal@capgemini.com>
2022-09-28 00:44:38 -0700

    updatedShoppingApp

--- CaseStudySprint2/UserProductCartTableViewController.swift
+++ CaseStudySprint2/UserProductCartTableViewController.swift
@@ -24,14 +24,18 @@ class UserProductCartTableViewController: UITableViewController {
     
     //MARK: Life cycle methods
     override func viewDidLoad() {
-        
+        print("load")
         super.viewDidLoad()
-        
         userCartArray = DBOperationsManager.dbManagerSharedInstance().fetchProductRecord()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        print("didappear")
+        super.viewDidAppear(animated)
+        userCartArray = DBOperationsManager.dbManagerSharedInstance().fetchProductRecord()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.reloadData()
